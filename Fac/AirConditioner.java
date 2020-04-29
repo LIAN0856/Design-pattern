513f8c2fa2c94d3247fc4687842cc7179e91a8b4
LIAN0856 <54886122+LIAN0856@users.noreply.github.com>
2020-04-29 14:36:15 +0800

    Facade Pattern

---
+++ Fac/AirConditioner.java
@@ -0,0 +1,15 @@
+package Fac;
+
+public class AirConditioner {
+
+	public void on() {
+		System.out.println("空气净化器打开！");
+		
+	}
+
+	public void off() {
+		System.out.println("空气净化器关上！");
+		
+	}
+
+}
---
+++ Fac/Television.java
@@ -0,0 +1,15 @@
+package Fac;
+
+public class Television {
+
+	public void on() {
+		System.out.println("电视机打开！");
+		
+	}
+
+	public void off() {
+		System.out.println("电视机关上！");
+		
+	}
+
+}
