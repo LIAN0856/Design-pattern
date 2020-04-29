1a3401752824c90936a664a9c4d1d3ba1687af2a
LIAN0856 <54886122+LIAN0856@users.noreply.github.com>
2020-04-29 14:26:17 +0800

    Chain Pattern

---
+++ chain/SmallFruitSorting.java
@@ -0,0 +1,15 @@
+package chain;
+
+public class SmallFruitSorting extends AbstractFruitSort {
+
+	public SmallFruitSorting(int weight) {
+		super(weight);
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void pushBox(String fruit) {
+		// TODO Auto-generated method stub
+        fruitBox.add("´ò¹ûÖ­£º"+fruit);
+	}
+
+}
