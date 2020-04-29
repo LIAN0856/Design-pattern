1fb9669637c27a36e6eccef2d269a7925396cb3e
LIAN0856 <54886122+LIAN0856@users.noreply.github.com>
2020-04-29 14:51:04 +0800

    State Pattern

---
+++ state/StateSet.java
@@ -0,0 +1,11 @@
+package state;
+
+public final class StateSet {
+
+	public static final int RUNNING = 0;
+	public static int NEW=1;
+	public static int RANNABLE=2;
+	public static int BLOCKED=4;
+	public static int DEAD=5;
+
+}
